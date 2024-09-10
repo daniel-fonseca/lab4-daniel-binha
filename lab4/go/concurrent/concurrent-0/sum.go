6342c257156df134249834605e4f2c029ad92147
CaioVFA <caio.araujo@ccc.ufcg.edu.br>
2024-09-10 15:41:39 -0300

    organizando diretorios

--- lab4/go/concurrent/concurrent-0/sum.go
+++ lab4/go/concurrent/concurrent-0/sum.go
@@ -1,98 +1,98 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"runtime"
-	"sync"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "runtime"
+    "sync"
 )
 
 // readFile reads a file from a given path and returns the content as a slice of bytes
 func readFile(filePath string) ([]byte, error) {
-	data, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
+    data, err := ioutil.ReadFile(filePath)
+    if err != nil {
+        return nil, err
+    }
+    return data, nil
 }
 
 func sum(filePath string) (int, error) {
-	data, err := readFile(filePath)
-	if err != nil {
-		return 0, err
-	}
+    data, err := readFile(filePath)
+    if err != nil {
+        return 0, err
+    }
 
-	_sum := 0
-	for _, b := range data {
-		_sum += int(b)
-	}
+    _sum := 0
+    for _, b := range data {
+        _sum += int(b)
+    }
 
-	return _sum, nil
+    return _sum, nil
 }
 
 func worker(id int, jobs <-chan string, results chan<- map[string]int, wg *sync.WaitGroup) {
-	defer wg.Done()
+    defer wg.Done()
 
-	for filePath := range jobs {
-		_sum, err := sum(filePath)
-		if err != nil {
-			results <- map[string]int{filePath: 0}
-			fmt.Printf("Error processing file %s: %v\n", filePath, err)
-		} else {
-			results <- map[string]int{filePath: _sum}
-		}
-	}
+    for filePath := range jobs {
+        _sum, err := sum(filePath)
+        if err != nil {
+            results <- map[string]int{filePath: 0}
+            fmt.Printf("Error processing file %s: %v\n", filePath, err)
+        } else {
+            results <- map[string]int{filePath: _sum}
+        }
+    }
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go <file1> <file2> ...")
-		return
-	}
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: go run main.go <file1> <file2> ...")
+        return
+    }
 
-	numWorkers := runtime.NumCPU()
-	jobs := make(chan string, numWorkers)
-	results := make(chan map[string]int, numWorkers)
+    numWorkers := runtime.NumCPU()
+    jobs := make(chan string, numWorkers)
+    results := make(chan map[string]int, numWorkers)
 
-	var wg sync.WaitGroup
+    var wg sync.WaitGroup
 
-	for i := 0; i < numWorkers; i++ {
-		wg.Add(1)
-		go worker(i, jobs, results, &wg)
-	}
+    for i := 0; i < numWorkers; i++ {
+        wg.Add(1)
+        go worker(i, jobs, results, &wg)
+    }
 
-	go func() {
-		for _, path := range os.Args[1:] {
-			jobs <- path
-		}
-		close(jobs)
-	}()
+    go func() {
+        for _, path := range os.Args[1:] {
+            jobs <- path
+        }
+        close(jobs)
+    }()
 
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
+    go func() {
+        wg.Wait()
+        close(results)
+    }()
 
-	sums := make(map[int][]string)
-	var totalSum int64
+    sums := make(map[int][]string)
+    var totalSum int64
 
-	for result := range results {
-		for file, _sum := range result {
-			if _sum == 0 {
-				continue
-			}
+    for result := range results {
+        for file, _sum := range result {
+            if _sum == 0 {
+                continue
+            }
 
-			totalSum += int64(_sum)
-			sums[_sum] = append(sums[_sum], file)
-		}
-	}
+            totalSum += int64(_sum)
+            sums[_sum] = append(sums[_sum], file)
+        }
+    }
 
-	fmt.Printf("%d\n", totalSum)
+    fmt.Printf("%d\n", totalSum)
 
-	for sum, files := range sums {
-		if len(files) > 1 {
-			fmt.Printf("Files with the same sum (%d): %v\n", sum, files)
-		}
-	}
+    for sum, files := range sums {
+        if len(files) > 1 {
+            fmt.Printf("Files with the same sum (%d): %v\n", sum, files)
+        }
+    }
 }
